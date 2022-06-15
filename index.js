c434f1b018f6335784e6aac3c399760d78cdd99f
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2022-06-15 09:25:56 +0200

    fix bugs sortinf and filtring at the same time

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const data = [
         lastTransactionDate: "Jan 12,2022",
         lastTransactionValue: "$783.22",
         status: "Done",
-        endDate: "Feb 23,2022",
-        totalUsed: "$1000"
+        endDate: "Aug 24,1991",
+        totalUsed: "$1000.00"
     },
     {
         logo: "./images/adobe.jpg",
@@ -47,13 +47,13 @@ const data = [
         link: "https://www.wix.com/",
         userTypeCard: "Visa",
         userCardNumber: "***** 2468",
-        userName: "Albus Dambldore",
-        userEmail: "perselvall31@gmail.com",
+        userName: "Minerva McGounuale",
+        userEmail: "kitty@gmail.com",
         lastTransactionDate: "Jan 12,2022",
         lastTransactionValue: "$783.22",
         status: "Pending",
         endDate: "Jun 2,2022",
-        totalUsed: "$783.22"
+        totalUsed: "$83.22"
     },
     {
         logo: "./images/youtube.jpg",
@@ -69,8 +69,7 @@ const data = [
         endDate: "Oct 12,2021",
         totalUsed: "$83.22"
     },
-
-        {
+    {
         logo: "./images/youtube.jpg",
         name: "Youtube",
         link: "https://www.youtube.com/",
@@ -83,6 +82,20 @@ const data = [
         status: "In Progress",
         endDate: "Jan 12,2022",
         totalUsed: "$711.67"
+    },
+    {
+        logo: "./images/wix.jpg",
+        name: "WIX",
+        link: "https://www.wix.com/",
+        userTypeCard: "Visa",
+        userCardNumber: "***** 2468",
+        userName: "Albus Dambldore",
+        userEmail: "perselvall31@gmail.com",
+        lastTransactionDate: "Jan 12,2022",
+        lastTransactionValue: "$783.22",
+        status: "Pending",
+        endDate: "Jun 2,2022",
+        totalUsed: "$783.22"
     },
 
 ];
@@ -121,8 +134,8 @@ function createTableHTML(data) {
                     </div>    
                 </td>    
                 <td class="doctitle">${data.name}</td>
-                <td class="card-type">${data.userTypeCard}</td>
-                <td class="user-name">${data.userName}</td>
+                <td class="card__type">${data.userTypeCard}</td>
+                <td class="user__name">${data.userName}</td>
                 <td class="last-trans-date">${data.lastTransactionDate}</td>
                 <td rowspan="2" class="status">
                     <div class="getstatus ${data.status === "Done" ? "green" : "yellow"}">
@@ -130,8 +143,8 @@ function createTableHTML(data) {
                         <span class="status__value">${data.status}</span>
                     </div>    
                 </td>    
-                <td rowspan="2" class="end-data">${data.endDate}</td>
-                <td rowspan="2" class="total-sum">${data.totalUsed}</td>
+                <td rowspan="2" class="end__data">${data.endDate}</td>
+                <td rowspan="2" class="totalsum">${data.totalUsed}</td>
                 <td rowspan="2">
                     <div class="dots">
                         <div class="dot"></div>
@@ -141,10 +154,10 @@ function createTableHTML(data) {
                 </td>    
             </tr>    
             <tr class="table__row secondary">
-                <td class="secondary-data">${data.link}</td>
-                <td class="secondary-data">${data.userCardNumber}</td>
-                <td class="secondary-data">${data.userEmail}</td>
-                <td class="secondary-data">${data.lastTransactionValue}</td>
+                <td class="secondary__data">${data.link}</td>
+                <td class="secondary__data">${data.userCardNumber}</td>
+                <td class="secondary__data">${data.userEmail}</td>
+                <td class="secondary__data">${data.lastTransactionValue}</td>
             </tr>    
         `
     )    
@@ -160,21 +173,26 @@ const values = {
 }
 
 let timer;
+let FILTRED_DATA = data;
+let SORTED_DATA = data;
 
 function changeField(fieldName, value) { 
     values[fieldName] = value;
     if (timer) {
         clearTimeout(timer) } 
-    timer = setTimeout(() => filterTable(data), 1000);
+    timer = setTimeout(() => filterTable(data), 0);
 }
 
+
 function filterTable(data) {
+
     const regexpName = new RegExp(values.name, 'gi');
     const regexpStatus = new RegExp(values.status, 'gi');
     const regexpUserName = new RegExp(values.userName, 'gi');
     const timeOfSet = new Date().getTimezoneOffset() * 60000;
     const firstDate = new Date(values.firstDate).getTime() + timeOfSet;
     const lastDate = new Date(values.lastDate).getTime() + timeOfSet;
+
     const filtredData = data.filter(row => {
         const dataEndDate = new Date(row.endDate).getTime();
         return row.name.match(regexpName) &&
@@ -184,64 +202,68 @@ function filterTable(data) {
             (isNaN(lastDate) || dataEndDate <= lastDate);
     });
 
+    FILTRED_DATA = filtredData;
     displayMatches(filtredData);
+    // sortTable();
+    refs.switchBtn.classList.contains("rotate") ? switchSortDirection() : sortTable()
 }
 
-function sortTable() { 
 
+function sortTable() { 
     switch(refs.sortInput.value) {
         case 'name': 
-            data.sort((a, b) => a.name.localeCompare(b.name));
-            displayMatches(data);
+            SORTED_DATA = FILTRED_DATA.sort((a, b) => a.name.localeCompare(b.name));
+            displayMatches(SORTED_DATA);
             break;
         case 'status':
-            data.sort((a, b) => a.status.localeCompare(b.status));
-            displayMatches(data);
+            SORTED_DATA = FILTRED_DATA.sort((a, b) => a.status.localeCompare(b.status));
+            displayMatches(SORTED_DATA);
             break;
         case 'user': 
-            data.sort((a, b) => a.userName.localeCompare(b.userName));
-            displayMatches(data);
+            SORTED_DATA = FILTRED_DATA.sort((a, b) => a.userName.localeCompare(b.userName));
+            displayMatches(SORTED_DATA);
             break;
         case 'date':
-            data.sort((a, b) => new Date(a.endDate).getTime() - new Date(b.endDate).getTime());
-            displayMatches(data);
+            SORTED_DATA = FILTRED_DATA.sort((a, b) => new Date(a.endDate).getTime() - new Date(b.endDate).getTime());
+            displayMatches(SORTED_DATA);
             break;
         case 'total used':
-            
-            data.sort((a, b) => Number.parseFloat((a.totalUsed).slice(1, a.totalUsed.length)) - Number.parseFloat((b.totalUsed).slice(1, b.totalUsed.length)))
-            displayMatches(data);
+            SORTED_DATA = FILTRED_DATA.sort((a, b) =>
+                Number.parseFloat((a.totalUsed).slice(1, a.totalUsed.length)) - Number.parseFloat((b.totalUsed).slice(1, b.totalUsed.length)))
+            displayMatches(SORTED_DATA);
             break;
         case '':
-            displayMatches(data);
+            
+            filterTable(data);
             break;
     }
 }
 
 function switchSortDirection() { 
-        switch(refs.sortInput.value) {
+    switch(refs.sortInput.value) {
         case 'name': 
-            data.sort((a, b) => b.name.localeCompare(a.name));
-            displayMatches(data);
+            SORTED_DATA = FILTRED_DATA.sort((a, b) => b.name.localeCompare(a.name));
+            displayMatches(SORTED_DATA);
             break;
         case 'status':
-            data.sort((a, b) => b.status.localeCompare(a.status));
-            displayMatches(data);
+            SORTED_DATA = FILTRED_DATA.sort((a, b) => b.status.localeCompare(a.status));
+            displayMatches(SORTED_DATA);
             break;
         case 'user':
-            data.sort((a, b) => b.userName.localeCompare(a.userName));
-            displayMatches(data);
+            SORTED_DATA = FILTRED_DATA.sort((a, b) => b.userName.localeCompare(a.userName));
+            displayMatches(SORTED_DATA);
             break;
         case 'date':
-            data.sort((a, b) => new Date(b.endDate).getTime() - new Date(a.endDate).getTime());
-            displayMatches(data);
+            SORTED_DATA = FILTRED_DATA.sort((a, b) => new Date(b.endDate).getTime() - new Date(a.endDate).getTime());
+            displayMatches(SORTED_DATA);
             break;
         case 'total used':
-            
-            data.sort((a, b) => Number.parseFloat((b.totalUsed).slice(1, b.totalUsed.length)) - Number.parseFloat((a.totalUsed).slice(1, a.totalUsed.length)))
-            displayMatches(data);
+            SORTED_DATA = FILTRED_DATA.sort((a, b) =>
+                    Number.parseFloat((b.totalUsed).slice(1, b.totalUsed.length)) - Number.parseFloat((a.totalUsed).slice(1, a.totalUsed.length)))
+            displayMatches(SORTED_DATA);
             break;
         case '':
-            displayMatches(data);
+            filterTable(data);
             break;
     }
 }
@@ -251,13 +273,14 @@ function displayMatches(data) {
     refs.tableBody.innerHTML = newTable;
 }
 
-
+//Event listners:
 refs.statusFilter.addEventListener('input', (e) => changeField("status", e.target.value));
 refs.searchInput.addEventListener('input', (e) => changeField("name", e.target.value));
 refs.employeeFilter.addEventListener('input', (e) => changeField("userName", e.target.value));
 refs.firstDateInput.addEventListener('input', (e) => changeField("firstDate", e.target.value));
 refs.lastDateInput.addEventListener('input', (e) => changeField("lastDate", e.target.value));
-refs.sortInput.addEventListener('input', (e) => sortTable(e));
+refs.sortInput.addEventListener('input', () => sortTable());
+
 refs.switchBtn.addEventListener('click', () => {
     refs.switchBtn.classList.toggle("rotate")
     refs.switchBtn.classList.contains("rotate") ? switchSortDirection() : sortTable()});
